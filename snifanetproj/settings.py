5c8b070f12ebb3dba0bc24e04b63d5e6f07a4bd9
jbwh <joelbawuah@hotmail.com>
2024-03-20 09:35:56 -0400

    update settings.py

--- snifanetproj/settings.py
+++ snifanetproj/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'snifanetproj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'catcollector',
+        'NAME': 'SniffaNet',
     }
 }
 
