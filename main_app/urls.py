35f7ed6a792ca67f7d859a9fd2a36f4dad8f1125
jbwh <joelbawuah@hotmail.com>
2024-03-20 15:16:34 -0400

    updated urls and migrated

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('reports/', views.ReportList.as_view(), name='reports_index'),
-    path('reports/<int:pk>/', views.ReportDetail.as_view(), name='reports_detail'),
+    path('reports/', views.reports_index, name='reports_index'),
+    path('reports/<int:pk>/', views.reports_detail, name='reports_detail'),
     path('reports/<int:pk>/delete/', views.ReportDelete.as_view(), name='reports_delete'),
 
 
